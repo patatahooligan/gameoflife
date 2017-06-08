e2339a5af99607c1d7418a97f2a0bc01f98f2497
patatahooligan <filwn.oikonomou@gmail.com>
2017-06-08 22:23:27 +0300

    Added turn(), cell() & newcell().

--- gameoflife/gridwrapper.cpp
+++ gameoflife/gridwrapper.cpp
@@ -5,6 +5,12 @@
 
 #include "gridwrapper.h"
 
+
+
+std::bitset<grid_size>::reference GridWrapper::new_cell(size_t x, size_t y) {
+	return (*new_grid)[y * grid_width + x];
+}
+
 GridWrapper::GridWrapper() :
 	// Default constructor for bitset sets all bits to zero
 	grid(new std::bitset<grid_size>) {}
@@ -22,16 +28,41 @@ void GridWrapper::reset() {
 
 void GridWrapper::turn() {
 	// Create a new bitset to hold the next state
-	std::unique_ptr<std::bitset<grid_size>> new_grid(new std::bitset<grid_size>);
+	new_grid.reset(new std::bitset<grid_size>);
 
 	// Iterate over all cells in the grid
-	for (size_t y=0; y<grid_height; y++) {
-		for (size_t x=0; x<grid_width; x++) {
-			// TODO: add code that determines next state of cell here
-			// syntax for current cell : (*grid)[y * grid_width + x]
+	for (size_t y=0; y<grid_height; ++y) {
+		for (size_t x=0; x<grid_width; ++x) {
+			// Boundary check
+			unsigned char neighbors = 0;
+			size_t minj = y-1;
+			if (y==0) minj = 0;
+			size_t maxj = y+1;
+			if (y==grid_height-1) maxj = grid_height-1;
+			for (size_t j = minj; j <= maxj; ++j) {
+				// Boundary check
+				size_t mini = x-1;
+				if (x==0) mini = 0;
+				size_t maxi = x+1;
+				if (x==grid_width-1) maxi = grid_width-1;
+				for (size_t i = mini; i <= maxi; ++i) {
+					if (i==j) continue;
+					neighbors += cell(i, j);
+				}
+			}
+			if (neighbors < 2 || neighbors > 3)
+				new_cell(x, y) = 0;
+			else if (neighbors == 3)
+				new_cell(x, y) = 1;
+			else
+				new_cell(x, y) = cell(x, y);
 		}
 	}
 
 	// Make grid point to the new state
-	grid.swap(new_grid);
+	grid = std::move(new_grid);
+}
+
+std::bitset<grid_size>::reference GridWrapper::cell(size_t x, size_t y) {
+	return (*grid)[y * grid_width + x];
 }
--- gameoflife/gridwrapper.h
+++ gameoflife/gridwrapper.h
@@ -10,12 +10,15 @@ const size_t
 
 class GridWrapper {
 	private:
-		std::unique_ptr<std::bitset<grid_size>> grid;
+		std::unique_ptr<std::bitset<grid_size>> grid, new_grid;
+		std::bitset<grid_size>::reference new_cell(size_t x, size_t y);
+
 	public:
 		GridWrapper();					// Allocate an empty grid
 		GridWrapper::~GridWrapper();	// Empty destructor
 		void reset();					// Set all grid cells to 0
 		void turn();					// Simulate a single turn of game of life
+		std::bitset<grid_size>::reference cell(size_t x, size_t y);
 
 		// TODO: add set() method to alter the state of single grid cells
 		//		 add method to render the grid

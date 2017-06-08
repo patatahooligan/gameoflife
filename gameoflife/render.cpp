80382d1a6ea9cd1b8e8a33482cae6e72558d68e3
patatahooligan <filwn.oikonomou@gmail.com>
2017-06-09 00:06:35 +0300

    Added code that draws cells in render()

--- gameoflife/render.cpp
+++ gameoflife/render.cpp
@@ -14,15 +14,16 @@ void render_init(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitWindowSize(window_width, window_height);
 	glutInitWindowPosition(-1, -1);
+	glutInitDisplayMode(GLUT_LUMINANCE | GLUT_DOUBLE);
 	glutCreateWindow("Game of Life");
 	glutDisplayFunc(render);
 	glutIdleFunc(idle);
 	gluOrtho2D(0.0, window_width, 0.0, window_height);
-	glutInitDisplayMode(GLUT_LUMINANCE | GLUT_DOUBLE);
 }
 
 void render() {
 	// Draw grid
+	glColor3f(0.3f, 0.3f, 0.3f);
 	glBegin(GL_LINES);
 	for (float x = window_width / grid_width; x < window_width; x += window_width / grid_width) {
 		glVertex2f(x, 0.0f);
@@ -35,7 +36,18 @@ void render() {
 	glEnd();
 
 	// Draw cells
-
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glBegin(GL_QUADS);
+	for (size_t y = 0; y < grid_height; ++y){
+		for (size_t x = 0; x < grid_width; ++x) {
+			if (!grid_wrapper.cell(x,y)) continue;
+			glVertex2f(x * window_width / grid_width, y * window_height / grid_height);
+			glVertex2f((x+1) * window_width / grid_width, y * window_height / grid_height);
+			glVertex2f((x+1) * window_width / grid_width, (y+1) * window_height / grid_height);
+			glVertex2f(x * window_width / grid_width, (y+1) * window_height / grid_height);
+		}
+	}
+	glEnd();
 	glutSwapBuffers();
 }
 
